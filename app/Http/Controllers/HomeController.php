e2f10c30fdcfb954d68f9f4ea470039222ab704e
Ayi <ayianjani774@gmail.com>
2022-02-21 20:48:25 +0700

    ganti nama function pada controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function halamanHome(){
+    public function home(){
         echo "Selamat Datang di Website Educationz";
     }
 }
