f9f417525ff39cf28df9b9bf4d987d88a802bb23
Ayi <ayianjani774@gmail.com>
2022-02-21 21:05:31 +0700

    update function

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,8 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+    public function baju(){
+        echo '<img src="https://cdn.elevenia.co.id/g/6/6/4/6/6/7/11664667_A3.jpg" alt="baju" height="500"/>';
+        echo '<h3 style="color: red;">Baju Olahraga Elastis Cantik</h3>';
+    }
+    public function rok(){
+        echo '<img src="https://filebroker-cdn.lazada.co.id/kf/H5265fb9d06644454acb8b077e829d4e3Y.jpg" alt="rok" heigt="500"/>';
+        echo '<h3 style="color: red;">Rok Olahraga Super Nyaman</h3>';
+    }
     public function sepatu(){
-        echo '<img src="https://cdn.elevenia.co.id/g/8/1/1/9/7/7/27811977_A1.jpg" alt="sepatu" height="100"/>';
-        echo '<p style="color: blue;">Sepatu Running Feminin Cantik</p>';
+        echo '<img src="https://cdn.elevenia.co.id/g/8/1/1/9/7/7/27811977_A1.jpg" alt="sepatu" height="500"/>';
+        echo '<h3 style="color: red;">Sepatu Running Feminim Cantik</h3>';
     }
 }
