3b9c557a5e3d5fff3be2da7d7f3f3bf7051236e4
Ayi <ayianjani774@gmail.com>
2022-02-21 21:38:47 +0700

    update nama web

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        echo "Selamat Datang di Website Educationz";
+        echo "Selamat Datang di Website Fashionizta";
     }
 }
