b72fc57f4f298e38a409f4eefc5a68cecd8f0b71
Ayi <ayianjani774@gmail.com>
2022-02-21 20:51:39 +0700

    membuat function sepatu

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function home(){
-        echo "Selamat Datang di Website Educationz";
+    public function sepatu(){
+        echo '<img src="https://cdn.elevenia.co.id/g/8/1/1/9/7/7/27811977_A1.jpg" alt="sepatu"/>';
     }
 }
