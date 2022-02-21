058c18152ff6da21273da6db34bb8c2d9ef52e18
Ayi <ayianjani774@gmail.com>
2022-02-21 20:38:16 +0700

    update nama function halamanHome()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function halamanHome(){
         echo "Selamat Datang di Website Educationz";
     }
 }
