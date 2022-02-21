d71d7debd635f6234fb8c203f42e7a0fefe66158
Ayi <ayianjani774@gmail.com>
2022-02-21 20:56:36 +0700

    perubahan isi function sepatu

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class ProductsController extends Controller
 {
     public function sepatu(){
-        echo '<img src="https://cdn.elevenia.co.id/g/8/1/1/9/7/7/27811977_A1.jpg" alt="sepatu"/>';
+        echo '<img src="https://cdn.elevenia.co.id/g/8/1/1/9/7/7/27811977_A1.jpg" alt="sepatu" height="100"/>';
+        echo '<p style="color: blue;">Sepatu Running Feminin Cantik</p>';
     }
 }
